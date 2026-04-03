e53f1778c7c4273c05723451319ab20e154fe66d
Rupam Haldar <rupam1.24@kgpian.iitkgp.ac.in>
2026-04-03 12:00:00 +0530

    Add Day 19 solutions

---
+++ Day-19/Problem-01/solution.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = list1 ? list1 : list2;
+        return dummy.next;
+    }
+};
+
+int main() {
+    ListNode* l1 = new ListNode(1);
+    ListNode* l2 = new ListNode(2);
+    Solution sol;
+    ListNode* res = sol.mergeTwoLists(l1, l2);
+    cout << res->val << " " << res->next->val << endl; // Expected: 1 2
+    return 0;
+}
