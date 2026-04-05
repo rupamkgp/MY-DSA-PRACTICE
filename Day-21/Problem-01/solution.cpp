667862b185c855a3dfc7684ed2118278d54a67de
Rupam Haldar <rupam1.24@kgpian.iitkgp.ac.in>
2026-04-05 12:00:00 +0530

    Add Day 21 solutions

---
+++ Day-21/Problem-01/solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    Solution sol;
+    cout << sol.middleNode(head)->val << endl; // Expected: 2
+    return 0;
+}
