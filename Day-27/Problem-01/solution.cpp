c85feef4b0b15de653a9c2bf7f73e0350942a5cb
Rupam Haldar <rupam1.24@kgpian.iitkgp.ac.in>
2026-04-11 12:00:00 +0530

    Add Day 27 solutions

---
+++ Day-27/Problem-01/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return l;
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 3, 5, 6};
+    Solution sol;
+    cout << sol.searchInsert(nums, 5) << endl; // Expected: 2
+    return 0;
+}
