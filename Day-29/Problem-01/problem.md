56b4ae1d1ffa202f71a2365d6e4a344bae7f930b
Rupam Haldar <rupam1.24@kgpian.iitkgp.ac.in>
2026-04-13 12:00:00 +0530

    Add Day 29 solutions

---
+++ Day-29/Problem-01/problem.md
@@ -0,0 +1,7 @@
+# 746. Min Cost Climbing Stairs
+
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
---
+++ Day-29/Problem-01/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int downOne = 0, downTwo = 0;
+        for (int i = 2; i <= n; i++) {
+            int temp = min(downOne + cost[i - 1], downTwo + cost[i - 2]);
+            downTwo = downOne;
+            downOne = temp;
+        }
+        return downOne;
+    }
+};
+
+int main() {
+    vector<int> cost = {10, 15, 20};
+    Solution sol;
+    cout << sol.minCostClimbingStairs(cost) << endl; // Expected: 15
+    return 0;
+}
