d1f10ca1e05a8c05ab09671889995c5a5a9024bd
Rupam Haldar <rupam1.24@kgpian.iitkgp.ac.in>
2026-05-04 12:00:00 +0530

    Add Day 50 solutions

---
+++ Day-50/Problem-01/solution.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+        return dummy.next;
+    }
+};
+
+int main() {
+    ListNode* l1 = new ListNode(2);
+    ListNode* l2 = new ListNode(5);
+    Solution sol;
+    ListNode* res = sol.addTwoNumbers(l1, l2);
+    cout << res->val << endl; // Expected: 7
+    return 0;
+}
